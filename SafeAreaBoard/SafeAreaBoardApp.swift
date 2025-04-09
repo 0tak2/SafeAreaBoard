0ec75072a20d20e2419caed63932eb96935b0cd0
0tak2 <0tak2.code@gmail.com>
2025-04-09 21:31:32 +0900

    Initial Commit

---
+++ SafeAreaBoard/SafeAreaBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeAreaBoardApp.swift
+//  SafeAreaBoard
+//
+//  Created by 임영택 on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SafeAreaBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
