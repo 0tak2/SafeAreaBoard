5e0f6d2b8302aa519019b6570f9d4430a0e028e9
0tak2 <0tak2.code@gmail.com>
2025-04-12 13:09:07 +0900

    style: 커스텀 컬러

--- SafeAreaBoard/Presentation/DesignSystem/Colors.swift
+++ SafeAreaBoard/Presentation/DesignSystem/Colors.swift
@@ -8,7 +8,14 @@
 import SwiftUI
 
 struct CustomColors {
-    static let primary: Color = .init(red: 255/255, green: 249/255, blue: 165/255)
-    static let primaryDarker1: Color = .init(red: 232/255, green: 203/255, blue: 56/255)
-    static let primaryDarker2: Color = .init(red: 153/255, green: 137/255, blue: 76/255)
+    static let primary: Color = .init(red: 244/255, green: 213/255, blue: 60/255)
+    static let primaryDarker1: Color = .init(red: 215/255, green: 183/255, blue: 24/255)
+    static let primaryDarker2: Color = .init(red: 202/255, green: 177/255, blue: 54/255)
+    static let primaryLighter: Color = .init(red: 248/255, green: 245/255, blue: 233/255)
+    static let warmGray: Color = .init(red: 202/255, green: 201/255, blue: 195/255)
+    static let warmGrayLigher1: Color = .init(red: 248/255, green: 248/255, blue: 244/255)
+    static let warmGrayLigher2: Color = .init(red: 248/255, green: 248/255, blue: 235/255)
+    static let warmGrayDarker1: Color = .init(red: 99/255, green: 99/255, blue: 96/255)
+    static let warmGrayDarker2: Color = .init(red: 57/255, green: 57/255, blue: 55/255)
+    static let warmWhite: Color = .init(red: 255/255, green: 255/255, blue: 252/255)
 }
