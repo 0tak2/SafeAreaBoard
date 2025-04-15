68676c7cd042fc1571a451a282ba9d026856236b
0tak2 <0tak2.code@gmail.com>
2025-04-15 19:38:47 +0900

    feat: 테스트 용 이메일 로그인

---
+++ SafeAreaBoard/Presentation/Onboarding/EmailLoginFormView.swift
@@ -0,0 +1,50 @@
+//
+//  EmailLoginFormView.swift
+//  SafeAreaBoard
+//
+//  Created by 임영택 on 4/15/25.
+//
+
+import SwiftUI
+
+/**
+ For debug/testing
+ */
+struct EmailLoginFormView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
+    private let authService: AuthServiceProtocol = AuthService(supabaseClient: SupabaseProvider.shared.supabase)
+    
+    var body: some View {
+        Form {
+            TextField(text: $email) {
+                Text("개발 계정 이메일")
+            }
+            .textInputAutocapitalization(.never)
+            
+            SecureField(text: $password) {
+                Text("개발 계정 비밀번호")
+            }
+            
+            Section {
+                Button {
+                    loginButtonTapped()
+                } label: {
+                    Text("로그인")
+                }
+            }
+
+        }
+    }
+    
+    func loginButtonTapped() {
+        Task {
+            do {
+                try await authService.signInWithEmail(email: email, password: password)
+            } catch {
+                print("로그인에 실패했습니다. \(error)")
+            }
+        }
+    }
+}
