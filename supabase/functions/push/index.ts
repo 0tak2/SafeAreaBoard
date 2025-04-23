90cb885769a80be8fd5032f3a78bef19c4462543
0tak2 <0tak2.code@gmail.com>
2025-04-23 16:24:52 +0900

    feat: 알림에 질문 내용, 좋아요 누른 사람 닉네임 포함

--- supabase/functions/push/index.ts
+++ supabase/functions/push/index.ts
@@ -1,6 +1,7 @@
 import { createClient } from 'npm:@supabase/supabase-js@2'
 import { JWT } from 'npm:google-auth-library@9'
 import serviceAccount from '../serviceAccountKey.json' with { type: 'json' }
+import * as postgres from 'https://deno.land/x/postgres@v0.17.0/mod.ts'
 
 interface Notification {
   id: string
@@ -18,28 +19,87 @@ interface WebhookPayload {
   schema: 'public'
 }
 
+const databaseUrl = Deno.env.get('SUPABASE_DB_URL')!
+
+const pool = new postgres.Pool(databaseUrl, 3, true)
+
 const supabase = createClient(
   Deno.env.get('SUPABASE_URL')!,
   Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
 )
 
+const getUserNickname = async (reactionId: number): Promise<string | undefined> => {
+  const connection = await pool.connect()
+
+  try {
+    const result = await connection.queryObject`
+      SELECT profiles.nickname FROM reactions
+        INNER JOIN profiles
+        ON reactions.profile_id = profiles.id
+      WHERE reactions.id = ${reactionId}
+    `
+    if (result.rows.length > 0) {
+      return result.rows[0]?.nickname
+    } else {
+      return undefined
+    }
+  } catch(error) {
+    console.error(`failed to fetch profile entity from db. ${error}`)
+    return undefined
+  }
+}
+
+const getQuestionTitle = async (questionId: number): Promise<string | undefined> => {
+  const connection = await pool.connect()
+
+  try {
+    const result = await connection.queryObject`SELECT * FROM questions WHERE id = ${questionId}`
+    if (result.rows.length > 0) {
+      return result.rows[0]?.content
+    } else {
+      return undefined
+    }
+  } catch(error) {
+    console.error(`failed to fetch question entity from db. ${error}`)
+    return undefined
+  }
+}
+
 Deno.serve(async (req) => {
   const payload: WebhookPayload = await req.json()
   const notif = payload.record
 
-  // 메시지 본문 생성 (notification_type에 따라 변형 가능)
-  const getMessageBody = (n: Notification): string => {
+  const getMessageTitle = (n: Notification): string => {
     switch (n.notification_type) {
       case 'NEW_QUESTION':
-        return '새로운 질문이 등록되었습니다!'
+        return '새로운 질문'
       case 'NEW_REACTION':
-        return '내 글에 누군가 반응했어요!'
+        return '새로운 반응'
+      default:
+        return 'SafeAreaBoard'
+    }
+  }
+
+  // 메시지 본문 생성 (notification_type에 따라 변형 가능)
+  const getMessageBody = async (n: Notification): Promise<string> => {
+    switch (n.notification_type) {
+      case 'NEW_QUESTION':
+        if (n.arguments?.question_id) {
+          return `Q.${await getQuestionTitle(n.arguments?.question_id as number)}`
+        }
+        return `새로운 질문이 등록되었습니다.`
+      case 'NEW_REACTION':
+        if (n.arguments?.post_id) {
+          return `내 글에 ${await getUserNickname(n.arguments?.reaction_id as number)}님이 하트를 눌렀어요.`
+        }
+        return `내 글에 누군가 반응했어요!`
       default:
         return '알림이 도착했습니다.'
     }
   }
 
-  const bodyText = getMessageBody(notif)
+  const bodyText = await getMessageBody(notif)
+  const titleText = await getMessageTitle(notif)
 
   // 푸시 대상 fcm_token 목록 가져오기
   let fcmTokens: string[] = []
@@ -89,7 +149,7 @@ Deno.serve(async (req) => {
           message: {
             token,
             notification: {
-              title: 'academy.safeAreaBoard',
+              title: titleText,
               body: bodyText,
             },
             data: {
@@ -104,7 +164,6 @@ Deno.serve(async (req) => {
     const resData = await res.json()
     if (res.status < 200 || res.status > 299) {
       console.error('FCM 에러', resData)
-      throw resData
     }
     responses.push(resData)
   }
